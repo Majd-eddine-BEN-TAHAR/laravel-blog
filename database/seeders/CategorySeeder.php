d1bb9a6052cf9241bd8fb320ff195bd3b039bc7e
= <majdeddinebentahar@gmail.com>
2024-01-03 22:02:16 +0100

    fix seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,23 +5,20 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
-use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run()
     {
         $categories = [
             ['name' => 'Travel', 'slug' => 'travel', 'description' => 'Travel tips and destination guides.'],
             ['name' => 'Technology', 'slug' => 'technology', 'description' => 'Latest tech trends and news.'],
-
         ];
 
         foreach ($categories as $category) {
-            Category::create($category);
+            if (!Category::where('slug', $category['slug'])->exists()) {
+                Category::create($category);
+            }
         }
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -6,27 +6,31 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        DB::table('users')->insert([
+        $users = [
             [
                 'name' => 'Admin User',
                 'email' => 'admin@example.com',
-                'password' => Hash::make('majdmajd'), // Replace 'password' with a secure password
+                'password' => Hash::make('majdmajd'),
                 'is_admin' => true,
             ],
             [
                 'name' => 'Regular User',
                 'email' => 'user@example.com',
-                'password' => Hash::make('majdmajd'), // Replace 'password' with a secure password
+                'password' => Hash::make('majdmajd'),
                 'is_admin' => false,
             ],
-        ]);
+        ];
+
+        foreach ($users as $user) {
+            if (!User::where('email', $user['email'])->exists()) {
+                User::create($user);
+            }
+        }
     }
 }
